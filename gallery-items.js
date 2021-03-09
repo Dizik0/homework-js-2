4a5c03bf33abf9b2eb5187c4f333906137c289a9
Dizik0 <deniska.didenko.01@mail.ru>
2021-03-10 02:02:21 +0300

    slider

--- gallery-items.js
+++ gallery-items.js
@@ -1,78 +1,9 @@
-const gallery = [
-    {
-        preview:
-            'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-        original:
-            'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-        description: 'Hokkaido Flower',
-    },
-    {
-        preview:
-            'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-        original:
-            'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-        description: 'Container Haulage Freight',
-    },
-    {
-        preview:
-            'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-        original:
-            'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-        description: 'Aerial Beach View',
-    },
-    {
-        preview:
-            'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-        original:
-            'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-        description: 'Flower Blooms',
-    },
-    {
-        preview:
-            'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-        original:
-            'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-        description: 'Alpine Mountains',
-    },
-    {
-        preview:
-            'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-        original:
-            'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-        description: 'Mountain Lake Sailing',
-    },
-    {
-        preview:
-            'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-        original:
-            'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-        description: 'Alpine Spring Meadows',
-    },
-    {
-        preview:
-            'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-        original:
-            'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-        description: 'Nature Landscape',
-    },
-    {
-        preview:
-            'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-        original:
-            'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-        description: 'Lighthouse Coast Sea',
-    },
-];
+import gallery from './gallery.js'
 const galleryEl = document.querySelector('.gallery')
-
 const createsHtmlGallery = createsLandscapePhotos(gallery)
-
 const boxModalOpen = document.querySelector('.lightbox')
-
 const imgModalEl = document.querySelector('.lightbox__image')
-
 const btnEl = document.querySelector('.lightbox__button')
-
 const closeOverlay = document.querySelector('.lightbox__overlay')
 
 galleryEl.insertAdjacentHTML('beforeend',createsHtmlGallery)
@@ -100,8 +31,9 @@ const openImageFn = (e) => {
 
     e.preventDefault()
 
-    const tegNameEl = e.target.nodeName
-    const protoNodeNameEl = e.target.dataset.source
+    let tegNameEl = e.target.nodeName
+    let protoNodeNameEl = e.target.dataset.source
+    let altPhotoEl = e.target.alt
     
     if (tegNameEl !=='IMG') {
         return
@@ -109,15 +41,19 @@ const openImageFn = (e) => {
 
     boxModalOpen.classList.add("is-open")
     imgModalEl.src = protoNodeNameEl
+    imgModalEl.alt = altPhotoEl
+    imgModalEl.dataset.index = e.target.dataset.index
+    imgModalEl.dataset.index = 0
 }
 
 galleryEl.addEventListener('click', openImageFn)
 
 const closeImageFn = () => {
-
     boxModalOpen.classList.remove("is-open")
-    imgModalEl.src = ''  
+    imgModalEl.src = ''
+    imgModalEl.alt = ''
 }
+
 btnEl.addEventListener('click', closeImageFn)
 
 closeOverlay.addEventListener('click', closeImageFn)
@@ -125,23 +61,46 @@ closeOverlay.addEventListener('click', closeImageFn)
 const closeEscapeFn = () => {
     console.log(event.key)
     if (event.key === 'Escape') {
-      boxModalOpen.classList.remove("is-open")
-    imgModalEl.src = ''
+      closeImageFn()
     }
-    
 }
 
-window.addEventListener('keydown',closeEscapeFn)
 
 
 
 
+window.addEventListener('keydown', (e) => {
+    const key = e.code
+if (key === 'Escape') {
+closeImageFn()
+}
+if (key === 'ArrowLeft') {
+    ArrowLeft()
+}
+if (key === 'ArrowRight') {
+    ArrowRight()
+}
+})
 
+const setNewSrc = (step, index) => {
+    imgModalEl.dataset.index = `${index + step}`
+    imgModalEl.src = gallery[index + step].original
+}
 
+const ArrowLeft = () => {
+    let index = +imgModalEl.dataset.index
+    if (index === 0) {
+        setNewSrc(0, gallery.length - 1)
+        return
+    }
+    setNewSrc(-1, index)
+}
 
-
-
-
-
-
-
+const ArrowRight = () => {
+    let index = +imgModalEl.dataset.index
+    if (index === gallery.length -1) {
+        setNewSrc(0, 0)
+        return
+    }
+    setNewSrc(1, index)
+}
